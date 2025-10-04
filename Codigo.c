a910e7861fcd915a7f67eff01795f8c9c61c99d5
Kevin Meireles <105472882+Kebions2077@users.noreply.github.com>
2025-10-04 18:50:03 -0300

    Update Codigo.c

--- Codigo.c
+++ Codigo.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
-int main(){
+
+int main(void){
+
 int a,x,b,c;
+
 a=1;
 b=1;
+c=0;
+x=0;
+
 c=a+b;
 c=c+3;
-for(x=0;x<10;x++){
+for(x=0;x<8;x++){
     c++;
 }
     return 0;
 }
+
