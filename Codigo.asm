444f5a9aa71d8fe9e4b0c139f6522df8120c3287
Kevin Meireles <105472882+Kebions2077@users.noreply.github.com>
2025-11-12 17:18:09 -0300

    Refactor Codigo.asm for improved logic and structure

--- Codigo.asm
+++ Codigo.asm
@@ -1,9 +1,9 @@
+
 .data
-a: .word 0
-b: .word 0
-c: .word 0
-x: .word 0
-resultado: .word 0
+    a: .word 0
+    b: .word 0
+    c: .word 0
+    resultado: .word 0
 
 .text
 main: 
@@ -11,27 +11,17 @@ main:
     lw $t0, 0($t7)
     la $t7, b
     lw $t1, 0($t7)
-    add $t2, $t0, $t1           # instrução aritmética entre registradores
+    add $t2, $t0, $t1
+    addi $t2, $t2, 3
     la $t7, c
     sw $t2, 0($t7)
-    addi $t3, $t2, 3            # instrução aritmética com imediato
-
-    # Inicializa c com 5 (sem alterar o .data)
-    li $t2, 5
-    la $t7, c
-    sw $t2, 0($t7)
-
+    li $t4, 4
 loop:
-    la $t7, c
-    lw $t2, 0($t7)              # load
-    li $t4, 6
-    beq $t2, $t4, fim_loop      # branch condicional: não tomado 1ª vez, tomado 2ª
+    lw $t2, 0($t7)
+    beq $t2, $t4, fim_loop
     addi $t2, $t2, 1
-    sw $t2, 0($t7)              # store
+    sw $t2, 0($t7)
     j loop
-
 fim_loop:
     la $t7, resultado
     sw $t2, 0($t7)
-    li $v0, 10
-    syscall
